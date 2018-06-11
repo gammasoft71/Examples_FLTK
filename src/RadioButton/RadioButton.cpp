6361af2d239266234acf15c1ef7b89a35f914838
gammasoft71 <gammasoft71@gmail.com>
2018-06-11 15:33:01 +0200

    Add RadioButton

---
+++ src/RadioButton/RadioButton.cpp
@@ -0,0 +1,44 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Round_Button.H>
+#include <FL/Fl_Window.H>
+
+class Form : public Fl_Window {
+public:
+  Form() : Fl_Window(200, 100, 300, 300, "RadioButton example") {
+    this->resizable(this);
+    
+    this->radioButton1.align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+    this->radioButton1.value(1);
+    this->radioButton1.callback([](Fl_Widget* sender, void* form) {
+      ((Form*)form)->radioButton1.value(1);
+      ((Form*)form)->radioButton2.value(0);
+      ((Form*)form)->radioButton3.value(0);
+    }, this);
+    
+    this->radioButton2.align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+    this->radioButton2.callback([](Fl_Widget* sender, void* form) {
+      ((Form*)form)->radioButton1.value(0);
+      ((Form*)form)->radioButton2.value(1);
+      ((Form*)form)->radioButton3.value(0);
+    }, this);
+
+    this->radioButton3.align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+    this->radioButton3.callback([](Fl_Widget* sender, void* form) {
+      ((Form*)form)->radioButton1.value(0);
+      ((Form*)form)->radioButton2.value(0);
+      ((Form*)form)->radioButton3.value(1);
+    }, this);
+  }
+  
+private:
+  Fl_Round_Button radioButton1 {30, 30, 110, 23, "radioButton 1"};
+  Fl_Round_Button radioButton2 {30, 60, 110, 23, "radioButton 2"};
+  Fl_Round_Button radioButton3 {30, 90, 110, 23, "radioButton 3"};
+};
+
+int main(int argc, char *argv[]) {
+  Form form;
+  form.show(argc, argv);
+  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
+  return Fl::run();
+}
