f818458f1cb335d1081d524be23fc596b3bcd7d3
gammasoft71 <gammasoft71@gmail.com>
2018-06-11 08:36:02 +0200

    Add Label

--- src/Label/Label.cpp
+++ src/Label/Label.cpp
@@ -7,7 +7,7 @@ public:
   Form() : Fl_Window(200, 100, 300, 300, "Label example") {
     this->resizable(this);
     
-    this->label1.align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+    this->label1.align(FL_ALIGN_LEFT | FL_ALIGN_TOP | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
   }
   
   Fl_Box label1 {10, 10, 75, 25, "label1"};
