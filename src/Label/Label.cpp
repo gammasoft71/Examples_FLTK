3e9638fd7133bef214ac1cc30d3975f1866b43ac
gammasoft71 <gammasoft71@gmail.com>
2018-06-11 08:15:05 +0200

    Create

---
+++ src/Label/Label.cpp
@@ -0,0 +1,21 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Window.H>
+
+class Form : public Fl_Window {
+public:
+  Form() : Fl_Window(200, 100, 640, 480, "Label example") {
+    this->resizable(this);
+    
+    this->label1.align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+  }
+  
+  Fl_Box label1 {10, 10, 75, 25, "label1"};
+};
+
+int main(int argc, char *argv[]) {
+  Form form;
+  form.show(argc, argv);
+  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
+  return Fl::run();
+}
