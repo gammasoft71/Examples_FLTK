92f289d80bc38dcdabe674e0674a335665a393b7
gammasoft71 <gammasoft71@gmail.com>
2018-06-11 08:28:38 +0200

    Add Helloworld

---
+++ src/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,24 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Window.H>
+
+class Form : public Fl_Window {
+public:
+  Form() : Fl_Window(200, 100, 300, 300, "My first application") {
+    this->resizable(this);
+    
+    this->label1.align(FL_ALIGN_LEFT | FL_ALIGN_TOP | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+    this->label1.labelfont(FL_HELVETICA_BOLD_ITALIC);
+    this->label1.labelsize(45);
+    this->label1.labelcolor(fl_rgb_color(0x00, 0x80, 0x00));
+  }
+  
+  Fl_Box label1 {5, 100, 290, 100, "Hello, World!"};
+};
+
+int main(int argc, char *argv[]) {
+  Form form;
+  form.show(argc, argv);
+  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
+  return Fl::run();
+}
