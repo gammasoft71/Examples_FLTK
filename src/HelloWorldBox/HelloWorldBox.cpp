fa87af0f59aa4bd557afb718944691426c2653a2
Gammasoft71 <gammasoft71@gmail.com>
2020-12-06 23:06:57 +0100

    Update HelloWorldBox.cpp

--- src/HelloWorldBox/HelloWorldBox.cpp
+++ src/HelloWorldBox/HelloWorldBox.cpp
@@ -3,22 +3,12 @@
 #include <FL/Fl_Window.H>
 
 namespace Examples {
-#if defined(__APPLE__)
-  float points_to_native_font_graphics_untit(float size) {
-    return size / 72.0f * 96.0f;  // font is in pixels and not in points
-  }
-#else
-  float points_to_native_font_graphics_untit(float size) {
-    return size;  // font is in points
-  }
-#endif
-
   class MainWindow : public Fl_Window {
   public:
     MainWindow() : Fl_Window(200, 100, 300, 300, "My first application") {
       box1.align(FL_ALIGN_CENTER | FL_ALIGN_INSIDE | FL_ALIGN_CLIP);
       box1.labelfont(FL_HELVETICA_BOLD_ITALIC);
-      box1.labelsize(points_to_native_font_graphics_untit(32));
+      box1.labelsize(36);
       box1.labelcolor(fl_rgb_color(0x00, 0x80, 0x00));
       
       resizable(this);
