8a9df781c128dd9a1cd5c44468e26422c0e2897f
Gammasoft71 <gammasoft71@gmail.com>
2020-12-08 00:43:25 +0100

    updatte Cursors

---
+++ src/Components/Cursors/Cursors.cpp
@@ -0,0 +1,61 @@
+#include <cstdint>
+#include <iostream>
+#include <initializer_list>
+#include <utility>
+#include <FL/Fl.H>
+#include <FL/Fl_Browser.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Window.H>
+#include "Fl_Test_Cursor.H"
+
+using namespace std;
+
+namespace Examples {
+  class Test_Zone : public Fl_Box {
+  public:
+    Test_Zone(int x, int y, int w, int h) : Fl_Box(x, y, w, h) {}
+    
+    void cursor(Fl_Cursor cursor) {cursor_= cursor;}
+    
+    int handle(int e) override {
+      switch(e) {
+        case FL_ENTER: window()->cursor(cursor_); return true;
+        case FL_LEAVE: window()->cursor(FL_CURSOR_DEFAULT); return true;
+        default: return Fl_Box::handle(e);
+      }
+    }
+    
+  private:
+    Fl_Cursor cursor_ = FL_CURSOR_DEFAULT;
+  };
+
+  class Main_Window : public Fl_Window {
+  public:
+    Main_Window() : Fl_Window(200, 100, 360, 240, "Cursors example") {
+      resizable(this);
+      for (auto item : initializer_list<pair<const char *, Fl_Cursor>> {{"Default", FL_CURSOR_DEFAULT}, {"Arrow", FL_CURSOR_ARROW}, {"Cross", FL_CURSOR_CROSS}, {"Wait", FL_CURSOR_WAIT}, {"Insert", FL_CURSOR_INSERT}, {"Hand", FL_CURSOR_HAND}, {"Help", FL_CURSOR_HELP}, {"Move", FL_CURSOR_MOVE}, {"North south", FL_CURSOR_NS}, {"West east", FL_CURSOR_WE}, {"North-west south-east", FL_CURSOR_NWSE}, {"North-est south-west", FL_CURSOR_NESW}, {"North", FL_CURSOR_N}, {"North east",FL_CURSOR_NE}, {"East", FL_CURSOR_E}, {"South east", FL_CURSOR_SE}, {"South", FL_CURSOR_S}, {"South west", FL_CURSOR_SW}, {"West", FL_CURSOR_W}, {"North west", FL_CURSOR_NW}, {"None", FL_CURSOR_NONE}})
+        cursors_list_box.add(item.first, reinterpret_cast<void*>(item.second));
+      cursors_list_box.type(FL_HOLD_BROWSER);
+      cursors_list_box.select(1);
+      cursors_list_box.callback([](Fl_Widget* sender, void* data) {
+        Main_Window* window = reinterpret_cast<Main_Window*>(data);
+        if (window->cursors_list_box.value() != 0)
+          window->test_zone.cursor(static_cast<Fl_Cursor>(reinterpret_cast<intptr_t>(window->cursors_list_box.data(window->cursors_list_box.value()))));
+      }, this);
+      
+      test_zone.box(FL_DOWN_BOX);
+      test_zone.color(FL_BACKGROUND2_COLOR);
+    }
+    
+  private:
+    Fl_Browser cursors_list_box {20, 20, 150, 200};
+    Test_Zone test_zone {190, 20, 150, 200};
+  };
+}
+
+int main(int argc, char *argv[]) {
+  Examples::Main_Window window;
+  window.show(argc, argv);
+  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
+  return Fl::run();
+}
