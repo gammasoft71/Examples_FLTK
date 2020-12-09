a7a13fdab84aa785ddc6092912cac94ec1479125
Gammasoft71 <gammasoft71@gmail.com>
2020-12-10 00:00:21 +0100

    move example

---
+++ src/Control_Containers/Window/Window.cpp
@@ -0,0 +1,37 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Button.H>
+#include <FL/fl_message.H>
+#include <FL/Fl_Window.H>
+
+namespace Examples {
+  class Main_Window : public Fl_Window {
+  public:
+    Main_Window() : Fl_Window(200, 100, 640, 480, "Window example") {
+      end();
+      resizable(this);
+
+      button.align(FL_ALIGN_CENTER | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+      button.callback([](Fl_Widget* sender, void* window) {
+        reinterpret_cast<Main_Window*>(window)->hide();
+      }, this);
+    }
+
+    void hide() override {
+      fl_message_icon()->label("?");
+      fl_message_title("Close window");
+      if (fl_choice("Are you sure you want exit?", "No", "Yes", nullptr) == 1)
+        Fl_Window::hide();
+    }
+    
+  private:
+    Fl_Button button {10, 10, 75, 25, "Close"};
+  };
+}
+
+int main(int argc, char *argv[]) {
+  Examples::Main_Window window;
+  window.show(argc, argv);
+  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
+  fl_message_hotspot(0);
+  return Fl::run();
+}
