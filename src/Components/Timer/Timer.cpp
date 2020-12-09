cd4fa4015cae93f5fa5e136c4df31aaab61e497e
Gammasoft71 <gammasoft71@gmail.com>
2020-12-10 00:37:58 +0100

    move examples

---
+++ src/Components/Timer/Timer.cpp
@@ -0,0 +1,51 @@
+#include <iomanip>
+#include <sstream>
+#include <FL/Fl.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Window.H>
+
+using namespace std;
+
+namespace Examples {
+  class Main_Window : public Fl_Window {
+  public:
+    Main_Window() : Fl_Window(200, 100, 230, 130, "Timer example") {
+      end();
+      resizable(this);
+      
+      button.callback([](Fl_Widget* sender, void* data) {
+        auto window = reinterpret_cast<Main_Window*>(data);
+        window->enable_timer = !window->enable_timer;
+        window->button.copy_label(window->enable_timer ? "Stop" : "Start");
+        if (window->enable_timer) Fl::add_timeout(0.1, on_timer_tick, data);
+      }, this);
+      
+      box.align(FL_ALIGN_LEFT | FL_ALIGN_TOP | FL_ALIGN_INSIDE | FL_ALIGN_CLIP);
+      box.labelfont(FL_HELVETICA_ITALIC);
+      box.labelsize(64);
+      box.labelcolor(fl_rgb_color(30, 144, 255));
+    }
+    
+  private:
+    static void on_timer_tick(void* data) {
+      auto window = reinterpret_cast<Main_Window*>(data);
+      stringstream stream;
+      stream << fixed << setprecision(1) << static_cast<double>(++window->timer) / 10;
+      window->box.copy_label(stream.str().c_str());
+      if (window->enable_timer) Fl::repeat_timeout(0.1, on_timer_tick, data);
+    }
+
+    Fl_Box box {10, 10, 210, 70, "0.0"};
+    Fl_Button button {10, 90, 75, 25, "Start"};
+    bool enable_timer = false;
+    int timer = 0;
+  };
+}
+
+int main(int argc, char *argv[]) {
+  Examples::Main_Window window;
+  window.show(argc, argv);
+  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
+  return Fl::run();
+}
