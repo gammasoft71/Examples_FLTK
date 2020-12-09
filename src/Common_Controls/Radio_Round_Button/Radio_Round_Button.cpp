86ee74e48c92c471f937352c127e6a7e3e088f61
Gammasoft71 <gammasoft71@gmail.com>
2020-12-09 23:26:06 +0100

    move examples

---
+++ src/Common_Controls/Radio_Round_Button/Radio_Round_Button.cpp
@@ -0,0 +1,51 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Radio_Round_Button.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Window.H>
+
+namespace Examples {
+  class Main_Window : public Fl_Window {
+  public:
+    Main_Window() : Fl_Window(200, 100, 300, 300, "Radio round button example") {
+      end();
+      resizable(this);
+
+      radio_round_button1.when(FL_WHEN_CHANGED);
+      radio_round_button1.callback([](Fl_Widget* sender, void* window) {
+        reinterpret_cast<Main_Window*>(window)->box1.label("Radio 1 is checked");
+      }, this);
+
+      radio_round_button2.value(true);
+      radio_round_button2.when(FL_WHEN_CHANGED);
+      radio_round_button2.callback([](Fl_Widget* sender, void* window) {
+        reinterpret_cast<Main_Window*>(window)->box1.label("Radio 2 is checked");
+      }, this);
+
+      radio_round_button3.when(FL_WHEN_CHANGED);
+      radio_round_button3.callback([](Fl_Widget* sender, void* window) {
+        reinterpret_cast<Main_Window*>(window)->box1.label("Radio 3 is checked");
+      }, this);
+
+      radio_round_button4.copy_label("Radio 4");
+      radio_round_button4.callback([](Fl_Widget* sender, void* window) {
+        reinterpret_cast<Main_Window*>(window)->box1.label("Radio 4 is checked");
+      }, this);
+
+      box1.align(FL_ALIGN_LEFT | FL_ALIGN_TOP | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+    }
+    
+  private:
+    Fl_Radio_Round_Button radio_round_button1 {30, 30, 110, 23, "Radio 1"};
+    Fl_Radio_Round_Button radio_round_button2 {30, 60, 110, 23, "Radio 2"};
+    Fl_Radio_Round_Button radio_round_button3 {30, 90, 110, 23, "Radio 3"};
+    Fl_Radio_Round_Button radio_round_button4 {30, 120, 110, 23};
+    Fl_Box box1{30, 150, 180, 23, "Radio 2 is checked"};
+  };
+}
+  
+int main(int argc, char *argv[]) {
+  Examples::Main_Window window;
+  window.show(argc, argv);
+  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
+  return Fl::run();
+}
---
+++ src/User_Controls/Radio_Toggle_Button/Radio_Toggle_Button.cpp
@@ -0,0 +1,60 @@
+#include <sstream>
+#include <FL/Fl.H>
+#include "Fl_Radio_Toggle_Button.h"
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Window.H>
+
+using namespace std;
+
+namespace Examples {
+  class Main_Window : public Fl_Window {
+  public:
+    Main_Window() : Fl_Window(200, 100, 300, 300, "Radio toggle button example") {
+      resizable(this);
+
+      radioButton1.auto_check(false);
+      radioButton1.state(FL_CHECKED);
+      radioButton1.callback([](Fl_Widget* sender, void* window) {
+        // Uncomments next line to check / uncheck radio button 1 (auto_check is false...)
+        //dynamic_cast<Fl_Radio_Toggle_Button*>(sender)->state(!dynamic_cast<Fl_Radio_Toggle_Button*>(sender)->state());
+      }, this);
+
+      radioButton2.state(FL_CHECKED);
+
+      radioButton3.when(FL_WHEN_CHANGED);
+      radioButton3.callback([](Fl_Widget* sender, void* window) {
+        stringstream ss;
+        ss << "Radio 3 checked = " << boolalpha << (dynamic_cast<Fl_Radio_Toggle_Button*>(sender)->state() == FL_CHECKED);
+        reinterpret_cast<Main_Window*>(window)->box1.copy_label(ss.str().c_str());
+      }, this);
+
+      radioButton4.toggle_button(true);
+      radioButton4.copy_label("Radio 4");
+
+      radioButton5.auto_check(false);
+      radioButton5.state(FL_CHECKED);
+      radioButton5.toggle_button(true);
+      radioButton5.callback([](Fl_Widget* sender, void* window) {
+        // Uncomments next line to check / uncheck radio button 5 (auto_check is false...)
+        //dynamic_cast<Fl_Radio_Toggle_Button*>(sender)->state(!dynamic_cast<Fl_Radio_Toggle_Button*>(sender)->state());
+      }, this);
+      
+      box1.align(FL_ALIGN_LEFT | FL_ALIGN_TOP | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+    }
+    
+  private:
+    Fl_Radio_Toggle_Button radioButton1 {30, 30, 110, 23, "Radio 1"};
+    Fl_Radio_Toggle_Button radioButton2 {30, 60, 110, 23, "Radio 2"};
+    Fl_Radio_Toggle_Button radioButton3 {30, 90, 110, 23, "Radio 3"};
+    Fl_Radio_Toggle_Button radioButton4 {30, 120, 110, 23};
+    Fl_Radio_Toggle_Button radioButton5 {30, 150, 110, 23, "Radio 5"};
+    Fl_Box box1{30, 180, 180, 23, "Radio 3 checked = false"};
+  };
+}
+
+int main(int argc, char *argv[]) {
+  Examples::Main_Window window;
+  window.show(argc, argv);
+  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
+  return Fl::run();
+}
