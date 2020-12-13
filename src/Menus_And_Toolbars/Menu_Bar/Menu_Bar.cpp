2606a6ebf9cdafe420d6fe6cd2e0b1407881a1f0
Gammasoft71 <gammasoft71@gmail.com>
2020-12-13 18:16:19 +0100

    Update Menu_Bar.cpp

--- src/Menus_And_Toolbars/Menu_Bar/Menu_Bar.cpp
+++ src/Menus_And_Toolbars/Menu_Bar/Menu_Bar.cpp
@@ -15,56 +15,55 @@ namespace Examples {
       resizable(text_display);
 
       text_display.buffer(&text_buffer);
-      text_display.show_cursor(0);
 
       Fl_Menu_Item items[] = {
         {"&File", 0, nullptr, nullptr, FL_SUBMENU},
-          {"&New", FL_COMMAND + 'n', &Main_Window::OnMenuItemClick, this},
-          {"&Open", FL_COMMAND + 'o', &Main_Window::OnMenuItemClick, this},
+          {"&New", FL_COMMAND + 'n', &Main_Window::on_menu_item_click, this},
+          {"&Open", FL_COMMAND + 'o', &Main_Window::on_menu_item_click, this},
           {"Open &recent", 0, nullptr, nullptr, FL_SUBMENU},
-            {"File 1", 0, &Main_Window::OnMenuItemClick, this},
-            {"File 2", 0, &Main_Window::OnMenuItemClick, this},
-            {"File 3", 0, &Main_Window::OnMenuItemClick, this},
-            {"File 4", 0, &Main_Window::OnMenuItemClick, this},
-            {"File 5", 0, &Main_Window::OnMenuItemClick, this},
+            {"File 1", 0, &Main_Window::on_menu_item_click, this},
+            {"File 2", 0, &Main_Window::on_menu_item_click, this},
+            {"File 3", 0, &Main_Window::on_menu_item_click, this},
+            {"File 4", 0, &Main_Window::on_menu_item_click, this},
+            {"File 5", 0, &Main_Window::on_menu_item_click, this},
             {0},
-          {"&Close", FL_COMMAND + 'w', &Main_Window::OnMenuItemClick, this, FL_MENU_DIVIDER},
-          {"&Save", FL_COMMAND + 's', &Main_Window::OnMenuItemClick, this},
-          {"Save &as...", 0, &Main_Window::OnMenuItemClick, this, FL_MENU_DIVIDER},
-          {"Page setup...", FL_COMMAND + FL_SHIFT + 'p', &Main_Window::OnMenuItemClick, this},
-          {"&Print", FL_COMMAND + 'p', &Main_Window::OnMenuItemClick, this, FL_MENU_DIVIDER},
-          {"&Quit", FL_COMMAND + 'q', &Main_Window::OnMenuItemClick, this},
+          {"&Close", FL_COMMAND + 'w', &Main_Window::on_menu_item_click, this, FL_MENU_DIVIDER},
+          {"&Save", FL_COMMAND + 's', &Main_Window::on_menu_item_click, this},
+          {"Save &as...", 0, &Main_Window::on_menu_item_click, this, FL_MENU_DIVIDER},
+          {"Page setup...", FL_COMMAND + FL_SHIFT + 'p', &Main_Window::on_menu_item_click, this},
+          {"&Print", FL_COMMAND + 'p', &Main_Window::on_menu_item_click, this, FL_MENU_DIVIDER},
+          {"&Quit", FL_COMMAND + 'q', &Main_Window::on_menu_item_click, this},
           {0},
         {"&Edit", 0, nullptr, nullptr, FL_SUBMENU},
-          {"&Undo", FL_COMMAND + 'z', &Main_Window::OnMenuItemClick, this},
-          {"&Redo", FL_COMMAND + FL_SHIFT + 'z', &Main_Window::OnMenuItemClick, this},
-          {"C&ut", FL_COMMAND + 'x', Main_Window::OnMenuItemClick, this},
-          {"&Copy", FL_COMMAND + 'c', Main_Window::OnMenuItemClick, this},
-          {"&Paste", FL_COMMAND + 'v', Main_Window::OnMenuItemClick, this, FL_MENU_DIVIDER},
-          {"Select &all", FL_COMMAND + 'a', Main_Window::OnMenuItemClick, this},
+          {"&Undo", FL_COMMAND + 'z', &Main_Window::on_menu_item_click, this},
+          {"&Redo", FL_COMMAND + FL_SHIFT + 'z', &Main_Window::on_menu_item_click, this},
+          {"C&ut", FL_COMMAND + 'x', Main_Window::on_menu_item_click, this},
+          {"&Copy", FL_COMMAND + 'c', Main_Window::on_menu_item_click, this},
+          {"&Paste", FL_COMMAND + 'v', Main_Window::on_menu_item_click, this, FL_MENU_DIVIDER},
+          {"Select &all", FL_COMMAND + 'a', Main_Window::on_menu_item_click, this},
           {0},
         {"&View", 0, nullptr, nullptr, FL_SUBMENU},
-          {"&Back", 0, Main_Window::OnMenuItemClick, this},
-          {"&Forward", 0, Main_Window::OnMenuItemClick, this, FL_MENU_DIVIDER},
-          {"&Show", 0, Main_Window::OnMenuItemClick, this},
-          {"&Hide", 0, Main_Window::OnMenuItemClick, this},
+          {"&Back", 0, Main_Window::on_menu_item_click, this},
+          {"&Forward", 0, Main_Window::on_menu_item_click, this, FL_MENU_DIVIDER},
+          {"&Show", 0, Main_Window::on_menu_item_click, this},
+          {"&Hide", 0, Main_Window::on_menu_item_click, this},
           {0},
         {"&Options", 0, nullptr, nullptr, FL_SUBMENU},
-          {"Option &A", FL_ALT + '1', Main_Window::OnMenuItemClick, this, FL_MENU_TOGGLE + FL_MENU_CHECK},
-          {"Option &B", FL_ALT + '2', Main_Window::OnMenuItemClick, this, FL_MENU_TOGGLE},
-          {"Option &C", FL_ALT + '3', Main_Window::OnMenuItemClick, this, FL_MENU_TOGGLE + FL_MENU_CHECK + FL_MENU_DIVIDER},
-          {"Option &D", FL_ALT + '4', Main_Window::OnMenuItemClick, this, FL_MENU_RADIO},
-          {"Option &E", FL_ALT + '5', Main_Window::OnMenuItemClick, this, FL_MENU_RADIO + FL_MENU_CHECK},
-          {"Option &F", FL_ALT + '6', Main_Window::OnMenuItemClick, this, FL_MENU_RADIO + FL_MENU_DIVIDER},
-          {"Option &G", FL_ALT + '7', Main_Window::OnMenuItemClick, this, FL_MENU_RADIO},
-          {"Option &H", FL_ALT + '8', Main_Window::OnMenuItemClick, this, FL_MENU_RADIO},
-          {"Option &I", FL_ALT + '9', Main_Window::OnMenuItemClick, this, FL_MENU_RADIO + FL_MENU_CHECK},
+          {"Option &A", FL_ALT + '1', Main_Window::on_menu_item_click, this, FL_MENU_TOGGLE + FL_MENU_CHECK},
+          {"Option &B", FL_ALT + '2', Main_Window::on_menu_item_click, this, FL_MENU_TOGGLE},
+          {"Option &C", FL_ALT + '3', Main_Window::on_menu_item_click, this, FL_MENU_TOGGLE + FL_MENU_CHECK + FL_MENU_DIVIDER},
+          {"Option &D", FL_ALT + '4', Main_Window::on_menu_item_click, this, FL_MENU_RADIO},
+          {"Option &E", FL_ALT + '5', Main_Window::on_menu_item_click, this, FL_MENU_RADIO + FL_MENU_CHECK},
+          {"Option &F", FL_ALT + '6', Main_Window::on_menu_item_click, this, FL_MENU_RADIO + FL_MENU_DIVIDER},
+          {"Option &G", FL_ALT + '7', Main_Window::on_menu_item_click, this, FL_MENU_RADIO},
+          {"Option &H", FL_ALT + '8', Main_Window::on_menu_item_click, this, FL_MENU_RADIO},
+          {"Option &I", FL_ALT + '9', Main_Window::on_menu_item_click, this, FL_MENU_RADIO + FL_MENU_CHECK},
           {0},
         {"&Help", 0, nullptr, nullptr, FL_SUBMENU},
-          {"Context", 0, Main_Window::OnMenuItemClick, this},
-          {"Index", 0, Main_Window::OnMenuItemClick, this},
-          {"Search", 0, Main_Window::OnMenuItemClick, this, FL_MENU_DIVIDER},
-          {"About", 0, Main_Window::OnMenuItemClick, this},
+          {"Context", 0, Main_Window::on_menu_item_click, this},
+          {"Index", 0, Main_Window::on_menu_item_click, this},
+          {"Search", 0, Main_Window::on_menu_item_click, this, FL_MENU_DIVIDER},
+          {"About", 0, Main_Window::on_menu_item_click, this},
           {0},
         {0}
       };
@@ -74,12 +73,12 @@ namespace Examples {
     }
     
   private:
-    static void OnMenuItemClick(Fl_Widget* sender, void* window) {reinterpret_cast<Main_Window*>(window)->OnMenuItemClick(dynamic_cast<Fl_Menu_Bar&>(*sender));}
-    void OnMenuItemClick(Fl_Menu_Bar& menu) {
+    static void on_menu_item_click(Fl_Widget* sender, void* window) {reinterpret_cast<Main_Window*>(window)->on_menu_item_click(dynamic_cast<Fl_Menu_Bar&>(*sender));}
+    void on_menu_item_click(Fl_Menu_Bar& menu) {
+      static int line_count = 0;
       text_buffer.append(menu.mvalue()->label());
       text_buffer.append("\n");
-      text_buffer.select(text_buffer.length(), text_buffer.length());
-      text_display.show_insert_position();
+      text_display.scroll(line_count++, 0);
       menu.value(0);
     }
     Fl_Text_Buffer text_buffer;
