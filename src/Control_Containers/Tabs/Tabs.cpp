03d9037fc37eb4fc81f79b85ab54299ea93fbedd
Gammasoft71 <gammasoft71@gmail.com>
2020-12-15 00:14:27 +0100

    Update Tabs.cpp

--- src/Control_Containers/Tabs/Tabs.cpp
+++ src/Control_Containers/Tabs/Tabs.cpp
@@ -8,7 +8,6 @@ using namespace std;
 class Main_Window : public Fl_Window {
 public:
   Main_Window() : Fl_Window(200, 100, 390, 270, "Tabs example") {
-
     tabs1 = make_unique<Fl_Tabs>(10, 10, 370, 250);
 
     page1 = make_unique<Fl_Group>(10, 30, 370, 230, "Page1");
@@ -19,11 +18,6 @@ public:
     
     page3 = make_unique<Fl_Group>(10, 30, 370, 230, "Page3");
     page3->end();
-
-    tabs1->end();
-
-    end();
-    resizable(this);
   }
   
 private:
@@ -36,6 +30,5 @@ private:
 int main(int argc, char *argv[]) {
   Main_Window window;
   window.show(argc, argv);
-  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
   return Fl::run();
 }
