d5b51bf443fbfbb48e44859d81b95af25fe77ce6
Gammasoft71 <gammasoft71@gmail.com>
2020-12-14 23:59:28 +0100

    Update Radio_Toggle_Button.cpp

--- src/User_Controls/Radio_Toggle_Button/Radio_Toggle_Button.cpp
+++ src/User_Controls/Radio_Toggle_Button/Radio_Toggle_Button.cpp
@@ -10,9 +10,6 @@ namespace Examples {
   class Main_Window : public Fl_Window {
   public:
     Main_Window() : Fl_Window(200, 100, 300, 300, "Radio toggle button example") {
-      end();
-      resizable(this);
-
       radioButton1.auto_check(false);
       radioButton1.state(FL_CHECKED);
       radioButton1.callback([](Fl_Widget* sender, void* window) {
@@ -56,6 +53,5 @@ namespace Examples {
 int main(int argc, char *argv[]) {
   Examples::Main_Window window;
   window.show(argc, argv);
-  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
   return Fl::run();
 }
