16632f7bd695a0b7f9765f5ebb2603811fba7b6a
Gammasoft71 <gammasoft71@gmail.com>
2020-12-14 23:41:36 +0100

    Update Output.cpp

--- src/Common_Controls/Output/Output.cpp
+++ src/Common_Controls/Output/Output.cpp
@@ -5,10 +5,7 @@
 namespace Examples {
   class Main_Window : public Fl_Window {
   public:
-    Main_Window() : Fl_Window(200, 100, 300, 300, "output example") {
-      end();
-      resizable(this);
-      
+    Main_Window() : Fl_Window(200, 100, 300, 300, "Output example") {
       output1.value("Output text");
     }
     
@@ -20,6 +17,5 @@ namespace Examples {
 int main(int argc, char *argv[]) {
   Examples::Main_Window window;
   window.show(argc, argv);
-  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
   return Fl::run();
 }
