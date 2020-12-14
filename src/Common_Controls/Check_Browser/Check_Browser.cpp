5854267e2c7e97c079d70863911e774b6936c0f9
Gammasoft71 <gammasoft71@gmail.com>
2020-12-14 23:36:42 +0100

    Update Check_Browser.cpp

--- src/Common_Controls/Check_Browser/Check_Browser.cpp
+++ src/Common_Controls/Check_Browser/Check_Browser.cpp
@@ -10,7 +10,6 @@ namespace Examples {
   class Main_Window : public Fl_Window {
   public:
     Main_Window() : Fl_Window(200, 100, 200, 240, "Check browser example") {
-      end();
       resizable(check_browser);
       
       for (auto index = 0; index < 10; index++)
@@ -38,6 +37,5 @@ namespace Examples {
 int main(int argc, char *argv[]) {
   Examples::Main_Window window;
   window.show(argc, argv);
-  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
   return Fl::run();
 }
