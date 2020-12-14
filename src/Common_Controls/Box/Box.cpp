9664a14d90a71ef969f0462c37454a20e2c07be7
Gammasoft71 <gammasoft71@gmail.com>
2020-12-14 23:29:18 +0100

    Update Box.cpp

--- src/Common_Controls/Box/Box.cpp
+++ src/Common_Controls/Box/Box.cpp
@@ -6,9 +6,6 @@ namespace Examples {
   class Main_Window : public Fl_Window {
   public:
     Main_Window() : Fl_Window(200, 100, 300, 300, "Box example") {
-      end();
-      resizable(this);
-      
       box1.align(FL_ALIGN_LEFT | FL_ALIGN_TOP | FL_ALIGN_INSIDE | FL_ALIGN_CLIP);
     }
     
@@ -20,6 +17,5 @@ namespace Examples {
 int main(int argc, char *argv[]) {
   Examples::Main_Window window;
   window.show(argc, argv);
-  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
   return Fl::run();
 }
