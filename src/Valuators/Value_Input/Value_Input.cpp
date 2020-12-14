d94bed53417955c38b71886126b3dd4b161228d4
Gammasoft71 <gammasoft71@gmail.com>
2020-12-15 00:06:08 +0100

    Update Value_Input.cpp

--- src/Valuators/Value_Input/Value_Input.cpp
+++ src/Valuators/Value_Input/Value_Input.cpp
@@ -12,8 +12,6 @@ namespace Examples {
   class Main_Window : public Fl_Window {
   public:
     Main_Window() : Fl_Window(200, 100, 300, 300, "Value input example") {
-      end();
-      
       value_input1.bounds(0, 1);
       value_input1.value(0.5);
       value_input1.step(0.01);
@@ -36,7 +34,6 @@ namespace Examples {
 }
 
 int main(int argc, char* argv[]) {
-  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
   Examples::Main_Window window;
   window.show(argc, argv);
   Fl::run();
