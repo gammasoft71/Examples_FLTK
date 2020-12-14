fd7065414ed5ea8d33525e15e51b8a0004c9a0bb
Gammasoft71 <gammasoft71@gmail.com>
2020-12-15 00:04:43 +0100

    Update Slider.cpp

--- src/Valuators/Slider/Slider.cpp
+++ src/Valuators/Slider/Slider.cpp
@@ -15,9 +15,6 @@ namespace Examples {
   class Main_Window : public Fl_Window {
   public:
     Main_Window() : Fl_Window(200, 100, 300, 340, "Slider example") {
-      end();
-      resizable(this);
-      
       hor_slider1.maximum(100);
       hor_slider1.callback(on_value_changed, &box1);
       hor_slider1.value(25);
@@ -79,6 +76,5 @@ namespace Examples {
 int main(int argc, char *argv[]) {
   Examples::Main_Window form;
   form.show(argc, argv);
-  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
   return Fl::run();
 }
