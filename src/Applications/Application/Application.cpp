259e25a679fa92e331b58cb018cabc307518c45d
Gammasoft71 <gammasoft71@gmail.com>
2020-12-14 23:27:22 +0100

    Update Application.cpp

--- src/Applications/Application/Application.cpp
+++ src/Applications/Application/Application.cpp
@@ -4,6 +4,5 @@
 int main(int argc, char *argv[]) {
   Fl_Window window(300, 300);
   window.show(argc, argv);
-  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
   return Fl::run();
 }
