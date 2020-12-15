9a2046f3469a0f6915c720b10f3b9be05916e607
Gammasoft <gammasoft71@gmail.com>
2020-12-15 22:59:55 +0100

    Update Test_Gui.cpp

--- src/Tests/Test_Gui/Test_Gui.cpp
+++ src/Tests/Test_Gui/Test_Gui.cpp
@@ -4,14 +4,12 @@
 namespace Examples {
   class Main_Window : public Fl_Window {
   public:
-    Main_Window() : Fl_Window(200, 100, 300, 300, "Test gui") {
-      
+    Main_Window() : Fl_Window(200, 100, 300, 300, "Test gui") { 
     }
   };
 }
 
 int main(int argc, char* argv[]) {
-  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
   Examples::Main_Window window;
   window.show(argc, argv);
   Fl::run();
