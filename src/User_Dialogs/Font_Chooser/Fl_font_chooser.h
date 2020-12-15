42f6edc7bdd4c5bd3042fb95132a006b8676c2a6
Gammasoft71 <gammasoft71@gmail.com>
2020-12-15 08:02:17 +0100

    Update Fl_font_chooser.h

--- src/User_Dialogs/Font_Chooser/Fl_font_chooser.h
+++ src/User_Dialogs/Font_Chooser/Fl_font_chooser.h
@@ -17,8 +17,8 @@ inline int fl_font_chooser(const char* name, Fl_Font& font, int& size, Fl_Color&
   class Fl_Font_Chooser {
   public:
     Fl_Font_Chooser() {
-      dialog.end();
-      int numberOfFont = Fl::set_fonts(fontSet ? fontSet->c_str() : nullptr);
+      dialog_.end();
+      int numberOfFont = Fl::set_fonts(font_set_ ? font_set_->c_str() : nullptr);
       for (int index = 0; index < numberOfFont; index++) {
         int fontType = 0;
         string fontName = Fl::get_font_name((Fl_Font)index, &fontType);
@@ -27,45 +27,50 @@ inline int fl_font_chooser(const char* name, Fl_Font& font, int& size, Fl_Color&
           if (fontType & FL_ITALIC) fontName= "@i" + fontName;
           if (fontName.find("@.") != string::npos) fontName.erase(fontName.find("@."), "@."s.length()); // Suppress subsequent formatting - some MS fonts have '@' in their name
         }
-        fontNames.add(fontName.c_str());
+        font_names_.add(fontName.c_str());
       }
       
-      fontNames.callback([](Fl_Widget* sender, void* fontChooser) {
-        ((Fl_Font_Chooser*)fontChooser)->currentFont = ((Fl_Font_Chooser*)fontChooser)->fontNames.value() - 1;
-        ((Fl_Font_Chooser*)fontChooser)->fontView.labelfont(((Fl_Font_Chooser*)fontChooser)->currentFont);
-        ((Fl_Font_Chooser*)fontChooser)->fontView.redraw();
+      font_names_.callback([](Fl_Widget* sender, void* data) {
+        auto font_chooser = reinterpret_cast<Fl_Font_Chooser*>(data);
+        font_chooser->font_ = font_chooser->font_names_.value() - 1;
+        font_chooser->font_view_.labelfont(font_chooser->font_);
+        font_chooser->font_view_.redraw();
       }, this);
       
       for (int index = 1; index <= 72; index++)
-        fontSizes.add(to_string(index).c_str());
+        font_sizes_.add(to_string(index).c_str());
       
-      fontSizes.callback([](Fl_Widget* sender, void* fontChooser) {
-        ((Fl_Font_Chooser*)fontChooser)->currentSize = ((Fl_Font_Chooser*)fontChooser)->fontSizes.value() - 1;
-        ((Fl_Font_Chooser*)fontChooser)->fontView.labelsize(((Fl_Font_Chooser*)fontChooser)->currentSize);
-        ((Fl_Font_Chooser*)fontChooser)->fontView.redraw();
+      font_sizes_.callback([](Fl_Widget* sender, void* data) {
+        auto font_chooser = reinterpret_cast<Fl_Font_Chooser*>(data);
+        font_chooser->size_ = font_chooser->font_sizes_.value() - 1;
+        font_chooser->font_view_.labelsize(font_chooser->size_);
+        font_chooser->font_view_.redraw();
       }, this);
       
-      fontView.box(FL_DOWN_BOX);
-      fontView.align(FL_ALIGN_LEFT | FL_ALIGN_TOP | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+      font_view_.box(FL_DOWN_BOX);
+      font_view_.align(FL_ALIGN_LEFT | FL_ALIGN_TOP | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
       
-      buttonOk.callback([](Fl_Widget* sender, void* fontChooser) {
-        ((Fl_Font_Chooser*)fontChooser)->result = 1;
-        ((Fl_Font_Chooser*)fontChooser)->dialog.hide();
+      button_ok_.callback([](Fl_Widget* sender, void* data) {
+        auto font_chooser = reinterpret_cast<Fl_Font_Chooser*>(data);
+        font_chooser->result_ = 1;
+        font_chooser->dialog_.hide();
       }, this);
       
-      buttonCancel.shortcut(FL_Escape);
-      buttonCancel.callback([](Fl_Widget* sender, void* fontChooser) {
-        ((Fl_Font_Chooser*)fontChooser)->result = 0;
-        ((Fl_Font_Chooser*)fontChooser)->dialog.hide();
+      button_cancel_.shortcut(FL_Escape);
+      button_cancel_.callback([](Fl_Widget* sender, void* data) {
+        auto font_chooser = reinterpret_cast<Fl_Font_Chooser*>(data);
+        font_chooser->result_ = 0;
+        font_chooser->dialog_.hide();
       }, this);
 
-      buttonColor.callback([](Fl_Widget* sender, void* fontChooser) {
+      button_color_.callback([](Fl_Widget* sender, void* data) {
+        auto font_chooser = reinterpret_cast<Fl_Font_Chooser*>(data);
         uchar r = 0, g = 0, b = 0;
-        Fl::get_color(((Fl_Font_Chooser*)fontChooser)->currentColor, r, g, b);
-        if (fl_color_chooser("Color", r, g, b, ((Fl_Font_Chooser*)fontChooser)->current_color_mode) != 0) {
-          ((Fl_Font_Chooser*)fontChooser)->currentColor = fl_rgb_color(r, g, b);
-          ((Fl_Font_Chooser*)fontChooser)->fontView.labelcolor(fl_rgb_color(r, g, b));
-          ((Fl_Font_Chooser*)fontChooser)->fontView.redraw();
+        Fl::get_color(font_chooser->color_, r, g, b);
+        if (fl_color_chooser("Color", r, g, b, font_chooser->color_mode_) != 0) {
+          font_chooser->color_ = fl_rgb_color(r, g, b);
+          font_chooser->font_view_.labelcolor(fl_rgb_color(r, g, b));
+          font_chooser->font_view_.redraw();
         }
       }, this);
     }
@@ -74,71 +79,71 @@ inline int fl_font_chooser(const char* name, Fl_Font& font, int& size, Fl_Color&
     Fl_Font_Chooser& operator=(const Fl_Font_Chooser&) = delete;
 
     int show() {
-      dialog.set_modal();
-      dialog.show();
-      while (dialog.visible())
+      dialog_.set_modal();
+      dialog_.show();
+      while (dialog_.visible())
         Fl::wait();
-      return result;
+      return result_;
     }
     
-    Fl_Font font() const {return currentFont;}
+    Fl_Font font() const {return font_;}
     void font(Fl_Font f) {
-      currentFont = f;
-      fontNames.value(currentFont + 1);
-      fontView.labelfont(currentFont);
+      font_ = f;
+      font_names_.value(font_ + 1);
+      font_view_.labelfont(font_);
     }
 
-    void font_set(const string& fs) {fontSet = make_unique<string>(fs);}
+    void font_set(const string& fs) {font_set_ = make_unique<string>(fs);}
     
-    string label() const {return dialog.label();}
-    void label(const string& l) {dialog.label(l.c_str());}
+    string label() const {return dialog_.label();}
+    void label(const string& l) {dialog_.label(l.c_str());}
     
-    Fl_Fontsize size() const {return currentSize;}
+    Fl_Fontsize size() const {return size_;}
     void size(Fl_Fontsize s) {
-      currentSize = s;
-      fontSizes.value(currentSize + 1);
-      fontView.labelsize(currentSize);
+      size_ = s;
+      font_sizes_.value(size_ + 1);
+      font_view_.labelsize(size_);
     }
     
-    Fl_Color color() const {return currentColor;}
+    Fl_Color color() const {return color_;}
     void color(int c) {
-      currentColor = c;
-      fontView.labelcolor(currentColor);
+      color_ = c;
+      font_view_.labelcolor(color_);
     }
     
-    int color_mode() const {return current_color_mode;}
+    int color_mode() const {return color_mode_;}
     void color_mode(int m) {
-      current_color_mode = m;
+      color_mode_ = m;
     }
     
   private:
-    Fl_Window dialog {100, 200, 550, 430, ""};
-    Fl_Hold_Browser fontNames {10, 10, 390, 170};
-    Fl_Hold_Browser fontSizes {410, 10, 130, 170};
-    Fl_Box fontView {10, 190, 530, 180, "The quick brown fox jumps over the lazy dog.\nTHE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."};
-    Fl_Return_Button buttonOk {295, 390, 75, 25, "Ok"};
-    Fl_Button buttonCancel {380, 390, 75, 25, "Cancel"};
-    Fl_Button buttonColor {465, 390, 75, 25, "Color..."};
+    Fl_Window dialog_ {100, 200, 550, 430, ""};
+    Fl_Hold_Browser font_names_ {10, 10, 390, 170};
+    Fl_Hold_Browser font_sizes_ {410, 10, 130, 170};
+    Fl_Box font_view_ {10, 190, 530, 180, "The quick brown fox jumps over the lazy dog.\nTHE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."};
+    Fl_Return_Button button_ok_ {295, 390, 75, 25, "Ok"};
+    Fl_Button button_cancel_ {380, 390, 75, 25, "Cancel"};
+    Fl_Button button_color_ {465, 390, 75, 25, "Color..."};
 
-    Fl_Font currentFont = FL_HELVETICA;
-    int currentSize = FL_NORMAL_SIZE;
-    Fl_Color currentColor = FL_FOREGROUND_COLOR;
-    unique_ptr<string> fontSet;
-    int current_color_mode = -1;
-    int result = 0;
-  } fontDialog;
+    Fl_Font font_ = FL_HELVETICA;
+    int size_ = FL_NORMAL_SIZE;
+    Fl_Color color_ = FL_FOREGROUND_COLOR;
+    unique_ptr<string> font_set_;
+    int color_mode_ = -1;
+    int result_ = 0;
+  } font_dialog;
   
-  fontDialog.font(font);
-  if (fontset) fontDialog.font_set(fontset);
-  fontDialog.label(name);
-  fontDialog.size(size);
-  fontDialog.color(color);
-  int result = fontDialog.show();
+  font_dialog.font(font);
+  if (fontset) font_dialog.font_set(fontset);
+  font_dialog.label(name);
+  font_dialog.size(size);
+  font_dialog.color(color);
+  int result_ = font_dialog.show();
   
-  if (result == 1) {
-    font = fontDialog.font();
-    size = fontDialog.size();
-    color = fontDialog.color();
+  if (result_ == 1) {
+    font = font_dialog.font();
+    size = font_dialog.size();
+    color = font_dialog.color();
   }
-  return result;
+  return result_;
 }
