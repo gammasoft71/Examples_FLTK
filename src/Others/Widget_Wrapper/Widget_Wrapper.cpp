41c0704cecb152e7467b161b3e128d6cab4d34a2
Gammasoft71 <gammasoft71@gmail.com>
2020-12-15 09:09:46 +0100

    Update Widget_Wrapper.cpp

--- src/Others/Widget_Wrapper/Widget_Wrapper.cpp
+++ src/Others/Widget_Wrapper/Widget_Wrapper.cpp
@@ -20,7 +20,6 @@ namespace Examples {
 }
 
 int main(int argc, char* argv[]) {
-  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
   Examples::Main_Window window;
   window.show(argc, argv);
   Fl::run();
