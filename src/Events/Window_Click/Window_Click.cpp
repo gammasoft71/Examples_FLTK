21befc9d3b6fc6b0636521cfa7cc676d969aaf0d
Gammasoft71 <gammasoft71@gmail.com>
2020-12-15 08:06:54 +0100

    Update Window_Click.cpp

--- src/Events/Window_Click/Window_Click.cpp
+++ src/Events/Window_Click/Window_Click.cpp
@@ -5,14 +5,12 @@
 namespace Examples {
   class Main_Window : public Fl_Window {
   public:
-    Main_Window() : Fl_Window(200, 100, 300, 300, "Click anywhere on the window") {
-      resizable(this);
-    }
+    Main_Window() : Fl_Window(200, 100, 300, 300, "Click anywhere on the window") {}
     
     int handle(int event) override {
       if (event == FL_RELEASE && Fl::event_button() == FL_LEFT_MOUSE) {
-        fl_message_title("Main_Window_Click");
         fl_message_hotspot(false);
+        fl_message_title("Main_Window_Click");
         fl_message("The window is clicked at {x=%d, y=%d}", Fl::event_x(), Fl::event_y());
       }
       return Fl_Window::handle(event);
@@ -23,7 +21,5 @@ namespace Examples {
 int main(int argc, char *argv[]) {
   Examples::Main_Window window;
   window.show(argc, argv);
-  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
-  fl_message_hotspot(0);
   return Fl::run();
 }
