8bbba6eab4ffc129ac8c1ad7f2dd908a39fb37e1
Gammasoft71 <gammasoft71@gmail.com>
2020-12-15 07:24:16 +0100

    Update Choice.cpp

--- src/Common_Dialogs/Choice/Choice.cpp
+++ src/Common_Dialogs/Choice/Choice.cpp
@@ -9,10 +9,10 @@ namespace Examples {
   class Main_Window : public Fl_Window {
   public:
     Main_Window() : Fl_Window(200, 100, 300, 300, "Choice example") {
-      button_show_choice.align(FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
       button_show_choice.callback([](Fl_Widget* sender, void* data) {
         auto window = reinterpret_cast<Main_Window*>(data);
         window->box_result.copy_label("Result = ");
+        fl_message_hotspot(0);
         fl_message_icon()->label("!");
         fl_message_icon()->color(fl_rgb_color(255, 255, 0));
         fl_message_icon()->labelcolor(fl_rgb_color(0, 0, 0));
@@ -35,6 +35,5 @@ namespace Examples {
 int main(int argc, char *argv[]) {
   Examples::Main_Window window;
   window.show(argc, argv);
-  fl_message_hotspot(0);
   return Fl::run();
 }
