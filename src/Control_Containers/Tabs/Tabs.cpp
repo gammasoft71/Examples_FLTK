02d715f3b25c55826cb85f338a89e0f12b19b8ed
Gammasoft71 <gammasoft71@gmail.com>
2020-12-17 11:24:46 +0100

    Update Tabs.cpp

--- src/Control_Containers/Tabs/Tabs.cpp
+++ src/Control_Containers/Tabs/Tabs.cpp
@@ -1,30 +1,20 @@
-#include <memory>
 #include <FL/Fl.H>
 #include <FL/Fl_Tabs.H>
 #include <FL/Fl_Window.H>
 
-using namespace std;
-
 class Main_Window : public Fl_Window {
 public:
-  Main_Window() : Fl_Window(200, 100, 390, 270, "Tabs example") {
-    tabs1 = make_unique<Fl_Tabs>(10, 10, 370, 250);
-
-    page1 = make_unique<Fl_Group>(10, 30, 370, 230, "Page1");
-    page1->end();
-    
-    page2 = make_unique<Fl_Group>(10, 30, 370, 230, "Page2");
-    page2->end();
-    
-    page3 = make_unique<Fl_Group>(10, 30, 370, 230, "Page3");
-    page3->end();
-  }
+  Main_Window() : Fl_Window(200, 100, 390, 270, "Tabs example") {}
   
 private:
-  unique_ptr<Fl_Tabs> tabs1;
-  unique_ptr<Fl_Group> page1;
-  unique_ptr<Fl_Group> page2;
-  unique_ptr<Fl_Group> page3;
+  Fl_Tabs tabs {10, 10, 370, 250};
+  Fl_Group page1 {10, 30, 370, 230, "Page1"};
+  Fl_End end_page1;
+  Fl_Group page2 {10, 30, 370, 230, "Page2"};
+  Fl_End end_page2;
+  Fl_Group page3 {10, 30, 370, 230, "Page3"};
+  Fl_End end_page3;
+  Fl_End end_tabs;
 };
 
 int main(int argc, char *argv[]) {
