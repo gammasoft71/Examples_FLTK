e12d4949b29b3da38715897c8d6e00f7c5740801
Gammasoft71 <gammasoft71@gmail.com>
2020-12-17 14:19:52 +0100

    Update Group_And_Check_Button.cpp

--- src/Others/Group_And_Check_Button/Group_And_Check_Button.cpp
+++ src/Others/Group_And_Check_Button/Group_And_Check_Button.cpp
@@ -1,44 +1,34 @@
-#include <memory>
 #include <FL/Fl.H>
 #include <FL/Fl_Check_Button.H>
 #include <FL/Fl_Scroll.H>
 #include <FL/Fl_Window.H>
 
-using namespace std;
-
 namespace Examples {
   class Main_Window : public Fl_Window {
   public:
     Main_Window() : Fl_Window(200, 100, 300, 160, "Group and Check button example") {
-      group1 = make_unique<Fl_Group>(10, 10, 135, 140, "Group 1");
-      group1->align(FL_ALIGN_INSIDE | FL_ALIGN_TOP | FL_ALIGN_LEFT);
-      group1->box(FL_BORDER_BOX);
+      group1.align(FL_ALIGN_INSIDE | FL_ALIGN_TOP | FL_ALIGN_LEFT);
+      group1.box(FL_FRAME_BOX);
       
-      check1 = make_unique<Fl_Check_Button>(20, 40, 104, 24, "check 1");
-      check2 = make_unique<Fl_Check_Button>(20, 70, 104, 24, "check 2");
-      check2->value(1);
-      check3 = make_unique<Fl_Check_Button>(20, 100, 104, 24, "check 3");
-      group1->end();
+      check2.value(1);
 
-      group2 = make_unique<Fl_Group>(155, 10, 135, 140, "Group 2");
-      group2->align(FL_ALIGN_INSIDE | FL_ALIGN_TOP | FL_ALIGN_LEFT);
-      group2->box(FL_BORDER_BOX);
-      check4 = make_unique<Fl_Check_Button>(165, 40, 104, 24, "check 4");
-      check5 = make_unique<Fl_Check_Button>(165, 70, 104, 24, "check 5");
-      check6 = make_unique<Fl_Check_Button>(165, 100, 104, 24, "check 6");
-      check6->value(1);
-      group2->end();
+      group2.align(FL_ALIGN_INSIDE | FL_ALIGN_TOP | FL_ALIGN_LEFT);
+      group2.box(FL_FRAME_BOX);
+
+      check6.value(1);
     }
     
   private:
-    unique_ptr<Fl_Group> group1;
-    unique_ptr<Fl_Group> group2;
-    unique_ptr<Fl_Check_Button> check1;
-    unique_ptr<Fl_Check_Button> check2;
-    unique_ptr<Fl_Check_Button> check3;
-    unique_ptr<Fl_Check_Button> check4;
-    unique_ptr<Fl_Check_Button> check5;
-    unique_ptr<Fl_Check_Button> check6;
+    Fl_Group group1 {10, 10, 135, 140, "Group 1"};
+    Fl_Check_Button check1 {20, 40, 104, 24, "check 1"};
+    Fl_Check_Button check2 {20, 70, 104, 24, "check 2"};
+    Fl_Check_Button check3 {20, 100, 104, 24, "check 3"};
+    Fl_End end_group1;
+    Fl_Group group2 {155, 10, 135, 140, "Group 2"};
+    Fl_Check_Button check4 {165, 40, 104, 24, "check 4"};
+    Fl_Check_Button check5 {165, 70, 104, 24, "check 5"};
+    Fl_Check_Button check6 {165, 100, 104, 24, "check 6"};
+    Fl_End end_group2;
   };
 }
 
