6a4a0539e85c4d579a05f45eb6dd1660ecdfd82b
Gammasoft71 <gammasoft71@gmail.com>
2020-12-17 20:48:20 +0100

    Update Multiline_Input.cpp

--- src/Common_Controls/Multiline_Input/Multiline_Input.cpp
+++ src/Common_Controls/Multiline_Input/Multiline_Input.cpp
@@ -5,14 +5,14 @@
 namespace Examples {
   class Main_Window : public Fl_Window {
   public:
-    Main_Window() : Fl_Window(200, 100, 300, 235, "Multiline input example") {
-      input1.value("Line 1\nLnine 2\nLine 3\nLine 4");
+    Main_Window() : Fl_Window(200, 100, 300, 300, "Multiline input example") {
+      input1.value("Line 1\nLnine 2\nLine 3\n...");
       input1.when(FL_WHEN_CHANGED);
       input1.callback([](Fl_Widget* sender, void* window) {
         reinterpret_cast<Main_Window*>(window)->input2.value(reinterpret_cast<Main_Window*>(window)->input1.value());
       }, this);
       
-      input2.value("Line 1\nLnine 2\nLine 3\nLine 4");
+      input2.value("Line 1\nLnine 2\nLine 3\n...");
       input2.when(FL_WHEN_CHANGED);
       input2.callback([](Fl_Widget* sender, void* window) {
         reinterpret_cast<Main_Window*>(window)->input1.value(reinterpret_cast<Main_Window*>(window)->input2.value());
@@ -20,8 +20,8 @@ namespace Examples {
     }
     
   private:
-    Fl_Multiline_Input input1 {10, 10, 280, 100};
-    Fl_Multiline_Input input2 {10, 125, 280, 100};
+    Fl_Multiline_Input input1 {10, 10, 280, 135};
+    Fl_Multiline_Input input2 {10, 155, 280, 135};
   };
 }
 
