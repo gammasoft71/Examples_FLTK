e3265713e5d71fa32e5e87c3f602db5bfb54aeee
Gammasoft71 <gammasoft71@gmail.com>
2020-12-17 13:37:36 +0100

    Update Colored_Tabs.cpp

--- src/Others/Colored_Tabs/Colored_Tabs.cpp
+++ src/Others/Colored_Tabs/Colored_Tabs.cpp
@@ -8,31 +8,23 @@ using namespace std;
 class Form : public Fl_Window {
 public:
   Form() : Fl_Window(200, 100, 390, 270, "Colored tabs example") {
-    tabs = make_unique<Fl_Tabs>(10, 10, 370, 250);
-
-    page_red = make_unique<Fl_Group>(10, 10, 370, 230, "Red");
-    page_red->color(FL_RED);
-    page_red->end();
-    
-    page_green = make_unique<Fl_Group>(10, 10, 370, 230, "Green");
-    page_green->color(FL_DARK_GREEN);
-    page_green->end();
-    
-    page_blue = make_unique<Fl_Group>(10, 10, 370, 230, "Blue");
-    page_blue->color(FL_BLUE);
-    page_blue->end();
-    
-    page_yellow = make_unique<Fl_Group>(10, 10, 370, 230, "Yellow");
-    page_yellow->color(FL_YELLOW);
-    page_yellow->end();
+    page_red.color(FL_RED);
+    page_green.color(FL_DARK_GREEN);
+    page_blue.color(FL_BLUE);
+    page_yellow.color(FL_YELLOW);
   }
   
 private:
-  unique_ptr<Fl_Tabs> tabs;
-  unique_ptr<Fl_Group> page_red;
-  unique_ptr<Fl_Group> page_green;
-  unique_ptr<Fl_Group> page_blue;
-  unique_ptr<Fl_Group> page_yellow;
+  Fl_Tabs tabs {10, 10, 370, 250};
+  Fl_Group page_red {10, 10, 370, 230, "Red"};
+  Fl_End end_page_red;
+  Fl_Group page_green {10, 10, 370, 230, "Green"};
+  Fl_End end_page_green;
+  Fl_Group page_blue {10, 10, 370, 230, "Blue"};
+  Fl_End end_page_blue;
+  Fl_Group page_yellow {10, 10, 370, 230, "Yellow"};
+  Fl_End end_page_yellow;
+  Fl_End end_tabs;
 };
 
 int main(int argc, char *argv[]) {
