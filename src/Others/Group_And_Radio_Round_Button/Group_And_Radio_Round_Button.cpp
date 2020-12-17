b97a2e57f11abd239dbd9a6b935bacfdd7dcd6e3
Gammasoft71 <gammasoft71@gmail.com>
2020-12-17 14:23:29 +0100

    Update Group_And_Radio_Round_Button.cpp

--- src/Others/Group_And_Radio_Round_Button/Group_And_Radio_Round_Button.cpp
+++ src/Others/Group_And_Radio_Round_Button/Group_And_Radio_Round_Button.cpp
@@ -1,45 +1,35 @@
-#include <memory>
 #include <FL/Fl.H>
 #include <FL/Fl_Radio_Round_Button.H>
 #include <FL/Fl_Radio_Button.H>
 #include <FL/Fl_Scroll.H>
 #include <FL/Fl_Window.H>
 
-using namespace std;
-
 namespace Examples {
   class Main_Window : public Fl_Window {
   public:
     Main_Window() : Fl_Window(200, 100, 300, 160, "Group and radio round button example") {
-      group1 = make_unique<Fl_Group>(10, 10, 135, 140, "Group 1");
-      group1->align(FL_ALIGN_INSIDE | FL_ALIGN_TOP | FL_ALIGN_LEFT);
-      group1->box(FL_BORDER_BOX);
+      group1.align(FL_ALIGN_INSIDE | FL_ALIGN_TOP | FL_ALIGN_LEFT);
+      group1.box(FL_FRAME_BOX);
       
-      radio1 = make_unique<Fl_Radio_Round_Button>(20, 40, 104, 24, "radio 1");
-      radio1->value(1);
-      radio2 = make_unique<Fl_Radio_Round_Button>(20, 70, 104, 24, "radio 2");
-      radio3 = make_unique<Fl_Radio_Round_Button>(20, 100, 104, 24, "radio 3");
-      group1->end();
+      radio1.value(1);
 
-      group2 = make_unique<Fl_Group>(155, 10, 135, 140, "Group 2");
-      group2->align(FL_ALIGN_INSIDE | FL_ALIGN_TOP | FL_ALIGN_LEFT);
-      group2->box(FL_BORDER_BOX);
-      radio4 = make_unique<Fl_Radio_Round_Button>(165, 40, 104, 24, "radio 4");
-      radio5 = make_unique<Fl_Radio_Round_Button>(165, 70, 104, 24, "radio 5");
-      radio5->value(1);
-      radio6 = make_unique<Fl_Radio_Round_Button>(165, 100, 104, 24, "radio 6");
-      group2->end();
+      group2.align(FL_ALIGN_INSIDE | FL_ALIGN_TOP | FL_ALIGN_LEFT);
+      group2.box(FL_FRAME_BOX);
+
+      radio5.value(1);
     }
     
   private:
-    unique_ptr<Fl_Group> group1;
-    unique_ptr<Fl_Group> group2;
-    unique_ptr<Fl_Radio_Round_Button> radio1;
-    unique_ptr<Fl_Radio_Round_Button> radio2;
-    unique_ptr<Fl_Radio_Round_Button> radio3;
-    unique_ptr<Fl_Radio_Round_Button> radio4;
-    unique_ptr<Fl_Radio_Round_Button> radio5;
-    unique_ptr<Fl_Radio_Round_Button> radio6;
+    Fl_Group group1 {10, 10, 135, 140, "Group 1"};
+    Fl_Radio_Round_Button radio1 {20, 40, 104, 24, "radio 1"};
+    Fl_Radio_Round_Button radio2 {20, 70, 104, 24, "radio 2"};
+    Fl_Radio_Round_Button radio3 {20, 100, 104, 24, "radio 3"};
+    Fl_End end_group1;
+    Fl_Group group2 {155, 10, 135, 140, "Group 2"};
+    Fl_Radio_Round_Button radio4 {165, 40, 104, 24, "radio 4"};
+    Fl_Radio_Round_Button radio5 {165, 70, 104, 24, "radio 5"};
+    Fl_Radio_Round_Button radio6 {165, 100, 104, 24, "radio 6"};
+    Fl_End end_group2;
   };
 }
 
