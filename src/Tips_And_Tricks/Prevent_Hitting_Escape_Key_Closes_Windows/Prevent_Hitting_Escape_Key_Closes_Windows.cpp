bb42a17448244de806b928a64ee555ef91bdc08d
Gammasoft71 <gammasoft71@gmail.com>
2020-12-19 23:43:17 +0100

    Update Prevent_Hitting_Escape_Key_Closes_Windows.cpp

--- src/Tips_And_Tricks/Prevent_Hitting_Escape_Key_Closes_Windows/Prevent_Hitting_Escape_Key_Closes_Windows.cpp
+++ src/Tips_And_Tricks/Prevent_Hitting_Escape_Key_Closes_Windows/Prevent_Hitting_Escape_Key_Closes_Windows.cpp
@@ -13,7 +13,9 @@ namespace Examples {
 
 int main(int argc, char* argv[]) {
   // The following line prevent hitting the 'Escape' key closes windows...
-  Fl::add_handler([](int event)->int {return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;});
+  Fl::add_handler([](int event)->int {
+    return event == FL_SHORTCUT && Fl::event_key() == FL_Escape;
+  });
   Examples::Main_Window window;
   window.show(argc, argv);
   Fl::run();
