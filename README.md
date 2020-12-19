5c22a8c9f6c6b1927b7046fcf78ca8126d8f940c
Gammasoft71 <gammasoft71@gmail.com>
2020-12-19 16:56:39 +0100

    add tile example

--- README.md
+++ README.md
@@ -71,6 +71,7 @@ Shows how to use Fltk controls only by programming code (c++17).
 * [Pack](src/Control_Containers/Pack) shows how to create a pack with Fl_Pack.
 * [Scroll](src/Control_Containers/Scroll) shows how to create a scroll with Fl_Scroll.
 * [Tabs](src/Control_Containers/Tabs) shows how to create a tabs and pages with Fl_Tabs and Fl_Group.
+* [Tile](src/Control_Containers/Tile) shows how to create a tile with Fl_Tile.
 * [Window](src/Control_Containers/Window) shows how to create a simple window with Fl_Window.
 * [Wizard](src/Control_Containers/Wizard) shows how to create a Wizard and pages with Fl_Wizard and Fl_Group.
 
---
+++ src/Control_Containers/Tile/Tile.cpp
@@ -0,0 +1,43 @@
+#include <memory>
+#include <FL/Fl.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Tile.H>
+#include <FL/Fl_Window.H>
+
+using namespace std;
+
+namespace Examples {
+  class Main_Window : public Fl_Window {
+  public:
+    Main_Window() : Fl_Window(200, 100, 300, 200, "Tile example") {
+      size_range(300, 200);
+      resizable(tile);
+      
+      box_red.color(fl_color_average(FL_RED, FL_WHITE, 0.5));
+      box_red.box(FL_FLAT_BOX);
+      
+      box_green.color(fl_color_average(FL_DARK_GREEN, FL_WHITE, 0.5));
+      box_green.box(FL_FLAT_BOX);
+      
+      box_blue.color(fl_color_average(FL_BLUE, FL_WHITE, 0.5));
+      box_blue.box(FL_FLAT_BOX);
+      
+      box_yellow.color(fl_color_average(FL_YELLOW, FL_WHITE, 0.5));
+      box_yellow.box(FL_FLAT_BOX);
+    }
+    
+  private:
+    Fl_Tile tile {0, 0, 300, 200};
+    Fl_Box box_red {0, 0, 200, 100};
+    Fl_Box box_green {0, 100, 100, 100};
+    Fl_Box box_blue {100, 100, 100, 100};
+    Fl_Box box_yellow {200, 0, 100, 200};
+    Fl_End end_tile;
+  };
+}
+
+int main(int argc, char *argv[]) {
+  Examples::Main_Window window;
+  window.show(argc, argv);
+  return Fl::run();
+}
