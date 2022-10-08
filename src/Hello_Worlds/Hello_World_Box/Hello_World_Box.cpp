6b8ff6cfd5766b27fe2527078ce1985932ad605a
Gammasoft <gammasoft71@gmail.com>
2022-10-08 11:39:10 +0200

    Update Hello_World_Box.cpp

--- src/Hello_Worlds/Hello_World_Box/Hello_World_Box.cpp
+++ src/Hello_Worlds/Hello_World_Box/Hello_World_Box.cpp
@@ -6,17 +6,18 @@ namespace Examples {
   class Main_Window : public Fl_Window {
   public:
     Main_Window() : Fl_Window(200, 100, 300, 300, "Hello world (box)") {
-      box1.align(FL_ALIGN_CENTER | FL_ALIGN_INSIDE | FL_ALIGN_CLIP);
-      box1.labelfont(FL_HELVETICA_BOLD_ITALIC);
+      box1.align(FL_ALIGN_CENTER | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
+      box1.labelfont(box1.labelfont() | FL_HELVETICA | FL_BOLD | FL_ITALIC);
       box1.labelsize(36);
       box1.labelcolor(fl_rgb_color(0x00, 0x80, 0x00));
-      
+      box1.labeltype(FL_SHADOW_LABEL);
+
       resizable(this);
       position((Fl::w() - w())/2, (Fl::h() - h())/2);
     }
     
   private:
-    Fl_Box box1 {5, 100, 290, 100, "Hello, World!"};
+    Fl_Box box1 {0, 0, 300, 300, "Hello, World!"};
   };
 }
 
