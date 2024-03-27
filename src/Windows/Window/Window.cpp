7b218bebfcdff9d4f221fa4dff303327ebb48b71
Gammasoft <gammasoft71@gmail.com>
2024-03-27 09:58:17 +0100

    Using auto

--- src/Windows/Window/Window.cpp
+++ src/Windows/Window/Window.cpp
@@ -6,7 +6,7 @@
 namespace Examples {
   class Main_Window : public Fl_Window {
   public:
-    Main_Window() : Fl_Window(200, 100, 640, 480, "Window example") {
+    Main_Window() : Fl_Window {200, 100, 640, 480, "Window example"} {
       button.align(FL_ALIGN_CENTER | FL_ALIGN_INSIDE | FL_ALIGN_CLIP | FL_ALIGN_WRAP);
       button.callback([](Fl_Widget* sender, void* window) {
         reinterpret_cast<Main_Window*>(window)->hide();
@@ -16,8 +16,7 @@ namespace Examples {
     void hide() override {
       fl_message_hotspot(false);
       fl_message_title("Close window");
-      if (fl_choice("Are you sure you want exit?", "No", "Yes", nullptr) == 1)
-        Fl_Window::hide();
+      if (fl_choice("Are you sure you want exit?", "No", "Yes", nullptr) == 1) Fl_Window::hide();
     }
     
   private:
@@ -25,8 +24,8 @@ namespace Examples {
   };
 }
 
-int main(int argc, char *argv[]) {
-  Examples::Main_Window window;
+auto main(int argc, char *argv[]) -> int {
+  auto window = Examples::Main_Window {};
   window.show(argc, argv);
   return Fl::run();
 }
